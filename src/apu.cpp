5f53f1336322e27d4d5713077edb0744015c7003
Mattias Lasersköld <mattias.l.sk@gmail.com>
2020-07-20 23:21:04 +0200

    Add registers to apu

--- src/apu.cpp
+++ src/apu.cpp
@@ -1,3 +1,23 @@
 
 
 #include "apu.h"
+
+bool matnes::Apu::write(uint16_t address, uint8_t value) {
+    if (inRange(address)) {
+        // Everything relative to the first register
+        registers.pulse1[address - 0x4000] = value;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+std::optional<uint8_t> matnes::Apu::read(uint16_t address) {
+    if (address == 0x4015) {
+        return registers.status;
+    }
+    else {
+        return {};
+    }
+}
--- src/apu.h
+++ src/apu.h
@@ -1,3 +1,35 @@
 #pragma once
 
-class Apu {};
+#include "bus.h"
+#include <cstdint>
+
+namespace matnes {
+
+class Apu : public IBusComponent {
+
+    // Try to write to component, return true if successfull
+    virtual bool write(uint16_t address, uint8_t value) override;
+
+    // Try to read a byte return empty if not in range
+    virtual std::optional<uint8_t> read(uint16_t address) override;
+
+private:
+    constexpr bool inRange(uint16_t address) {
+        return address >= 0x4000 && address < 0x4020;
+    }
+
+    struct {
+        // start at 0x4000
+        uint8_t pulse1[4] = {};   // 0x0-0x3
+        uint8_t pulse2[4] = {};   // 0x4-0x7
+        uint8_t triangle[4] = {}; // 0x8-0xB
+        uint8_t noise[4] = {};    // 0xC-0xF
+        uint8_t dmc[4] = {};      // 0x10-0x13
+        uint8_t unused = 0;       // 0x14
+        uint8_t status = 0;       // 0x15
+        uint8_t unused2 = 0;      // 0x16
+        uint8_t frameCounter = 0; // 0x17
+    } registers;
+};
+
+} // namespace matnes
