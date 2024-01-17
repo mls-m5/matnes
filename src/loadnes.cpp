94ce72be8080d6ed4827d8cf5e8ce6adc65bbd7c
Mattias Lasersköld <mattias.l.sk@gmail.com>
2024-01-17 16:38:13 +0100

    Small updates to load functions

--- src/loadnes.cpp
+++ src/loadnes.cpp
@@ -1,6 +1,9 @@
 
 #include "loadnes.h"
+#include <bit>
+#include <filesystem>
 #include <fstream>
+#include <stdexcept>
 
 namespace matnes {
 namespace cartrige {
@@ -11,19 +14,37 @@ RomData loadRom(std::istream &stream) {
     stream.read(reinterpret_cast<char *>(&data.header),
                 sizeof(RomData::Header));
 
-    data.progRomData.resize(data.header.getProgRomSize());
+    data.progRomData.resize(data.header.getProgRomSize() * romUnits);
 
     stream.read(data.progRomData.data(), data.progRomData.size());
 
     return data;
 }
 
-RomData loadRom(const std::string &filename) {
-    std::fstream file(filename);
+RomData loadRom(std::filesystem::path path) {
+    std::fstream file(path);
     if (!file.is_open()) {
-        throw std::runtime_error(filename + " could not be opened");
+        throw std::runtime_error(path.string() + " could not be opened");
     }
     return loadRom(file);
+}
+
+bool saveRom(const RomData &rom, std::filesystem::path path) {
+    auto file = std::ofstream{path};
+    if (!file.is_open()) {
+        throw std::runtime_error{path.string() + " could not be saved"};
+    }
+    return saveRom(rom, file);
+}
+
+bool saveRom(const RomData &rom, std::ostream &stream) {
+    auto header = rom.header;
+    header.romSize = rom.progRomData.size() / romUnits;
+
+    stream.write(std::bit_cast<char *>(&header), sizeof(header));
+    stream.write(rom.progRomData.data(), rom.progRomData.size());
+
+    return true;
 }
 
 } // namespace cartrige
--- src/loadnes.h
+++ src/loadnes.h
@@ -1,17 +1,23 @@
 #pragma once
 
 #include <cstdint>
+#include <filesystem>
 #include <iosfwd>
 #include <string>
 
 namespace matnes {
 namespace cartrige {
 
+constexpr size_t romUnits = 16 * 1024;
+constexpr size_t chRomUnits = 8 * 1024;
+
 struct RomData {
     // Structure for INES 2.0
     struct Header {
         uint8_t startBytes[4] = {};
-        uint8_t romSize = 0;   // 4
+        // Rom size is in 16KB units
+        uint8_t romSize = 0; // 4
+        // chRomSize is in 8KB units
         uint8_t chRomSize = 0; // 5
         uint8_t flags6 = 0;
         uint8_t flags7 = 0;
@@ -33,7 +39,10 @@ struct RomData {
 };
 
 RomData loadRom(std::istream &stream);
-RomData loadRom(const std::string &filename);
+RomData loadRom(std::filesystem::path);
+
+bool saveRom(const RomData &rom, std::ostream &stream);
+bool saveRom(const RomData &rom, std::filesystem::path);
 
 } // namespace cartrige
 } // namespace matnes
